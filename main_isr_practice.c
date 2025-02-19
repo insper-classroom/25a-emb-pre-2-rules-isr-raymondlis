caa170feff46a824c06a3ed497e1651b7d75e7f5
raymondlis <raylisbona@gmail.com>
2025-02-19 17:05:00 -0300

    t3

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -6,25 +6,12 @@ const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
 
-    // ERRO: addon IsrPrintf
-    printf("btn pressed \n");
-
-    // ERRO: addon IsrNoLoop
-    while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
-    }
-
-    // ERRO: addon IsrPrintf
-    printf("btn released \n");
-
-    // ERRO: addon IsrNoDelay
-    sleep_ms(1);
+    
     btn_flag = 1;
   }
 }
@@ -41,7 +28,22 @@ int main() {
   int a;
   while (1) {
     if (btn_flag) {
+        // ERRO: addon IsrPrintf
+      printf("btn pressed \n");
+
+      // ERRO: addon IsrNoLoop
+      while (!pio_get(BTN_PIN_R)) {
+        // ERRO: addon IsrNoDelay
+        sleep_ms(1);
+      }
+
+      // ERRO: addon IsrPrintf
+      printf("btn released \n");
+
+      // ERRO: addon IsrNoDelay
+      sleep_ms(1);
       capture_flag = 1;
+
       btn_flag = 0;
     }
 
